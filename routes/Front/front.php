6185247c57a8a1551840c4bf3a23169b101a1915
Ko Phyo <myohanhtet.ly@gmail.com>
2022-01-11 12:34:45 +0630

    add public url

--- routes/Front/front.php
+++ routes/Front/front.php
@@ -4,6 +4,8 @@ Route::get('/',function (){
     return view('welcome');
 });
 
+Route::get('/','HomeController@index');
+
 //Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/lang/{locale}','LocalizationController@lang')->name('lang');
